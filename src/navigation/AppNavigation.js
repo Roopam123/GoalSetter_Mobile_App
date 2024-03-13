a41dbe7309c7e7a0693f0346a6fff031e4ce0548
Roopam <roopamr2411@gmail.com>
2024-03-13 15:37:44 +0530

    project setup

---
+++ src/navigation/AppNavigation.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AppNavigation = () => {
+  return (
+    <View>
+      <Text>AppNavigation Our </Text>
+    </View>
+  )
+}
+
+export default AppNavigation
